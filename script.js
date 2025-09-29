0f2264c0ffc8d3aa6fe3ed5405c5c32cf2317c6d
Maheshrkumar22 <54180607+Maheshrkumar22@users.noreply.github.com>
2025-09-30 03:15:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ document.getElementById('searchBtn').addEventListener('click', async () => {
         // Real API call (replace mockApiResponse)
         loadingSpinner.textContent = 'Loading...';
         loadingSpinner.classList.remove('hidden');
-        const response = await fetch(`http://127.0.0.1/api/${encodeURIComponent(url)}`);
+        const response = await fetch(`https://fastapi-python-boilerplate.onrender.com/api/${encodeURIComponent(url)}`);
         const data = await response.json();
         
 
